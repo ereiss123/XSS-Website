e82506314f838a46ff15fbe6e9e0c33762153bfc
Eric Reiss <reiss_eric@hotmail.com>
2025-06-14 14:35:07 +0000

    remove flags from main.py

--- main.py
+++ main.py
@@ -5,12 +5,6 @@ import os
 app = Flask(__name__)
 app.jinja_env.globals["Markup"] = Markup
 
-with open("flags/challenge1.txt") as f1, open("flags/challenge2.txt") as f2:
-    FLAG1 = f1.read().strip()
-    FLAG2 = f2.read().strip()
-
-exfiltrated = []
-
 @app.route("/")
 def index():
     return redirect(url_for("login"))
@@ -19,4 +13,4 @@ def index():
 def login():
     query1 = request.args.get("input_user", "")
     query2 = request.args.get("input_pass", "")
-    return render_template("challenge1.html", user_input1=query1, user_input2=query2, flag=FLAG1)
+    return render_template("challenge1.html", user_input1=query1, user_input2=query2)
